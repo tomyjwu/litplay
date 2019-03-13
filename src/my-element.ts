0338db3f211c53c322d6a4857f2882107ab06101
Tom Wu <tom.wu@genie-networks.com>
2019-03-13 18:35:05 +0800

    demo backend data

--- src/my-element.ts
+++ src/my-element.ts
@@ -3,19 +3,28 @@ import { LitElement, html, customElement, property } from 'lit-element';
 @customElement('my-element')
 export class MyElement extends LitElement {
     @property()
-    foo = 'foo';
+    foo = {"firstName":"Jayce","lastName":"Hauck","username":"Domenic_Kuhn81","email":"Gladyce.Weber@yahoo.com"};
 
     render() {
-        return html`<p>${this.foo}</p>`;
+    return html`
+    <style>
+    .s1 {
+        color: blue;
     }
-
+    </style>
+    <h1 class="s1">${this.foo.firstName}</h1>
+    <h2>${this.foo.lastName}</h2>
+    <p>${this.foo.email}</p>`;
+    }
     firstUpdated(changedProperties:any) {
         changedProperties.forEach((oldValue:any , propName:any) => {
           console.log(`${propName} changed. oldValue: ${oldValue}`);
         });
         fetch('api/user')
             .then((response) => response.json())
-            .then((bodyRes) => console.log(bodyRes));
+            .then((bodyRes) => {
+                this.foo = bodyRes;
+            });
 
     }
 }
