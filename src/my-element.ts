4532a9e35f90e49ab8dd9165932e1d1dda8d4959
Tom Wu <tom.wu@genie-networks.com>
2019-05-31 15:36:50 +0800

    added sam refresh

--- src/my-element.ts
+++ src/my-element.ts
@@ -4,7 +4,7 @@ import { LitElement, html, customElement, property } from 'lit-element';
 export class MyElement extends LitElement {
     @property()
     foo = {"firstName":"Jayce","lastName":"Hauck","username":"Domenic_Kuhn81","email":"Gladyce.Weber@yahoo.com"};
-
+    @property() bar = {year:1969, month:8}
     render() {
     return html`
     <style>
@@ -14,7 +14,9 @@ export class MyElement extends LitElement {
     </style>
     <h1 class="s1">${this.foo.firstName}</h1>
     <h2>${this.foo.lastName}</h2>
-    <p>${this.foo.email}</p>`;
+    <p>${this.foo.email}</p>
+    <p>${this.bar.year}</p>`;
+    
     }
     firstUpdated(changedProperties:any) {
         changedProperties.forEach((oldValue:any , propName:any) => {
@@ -26,5 +28,17 @@ export class MyElement extends LitElement {
                 this.foo = bodyRes;
             });
 
+    }
+    constructor() {
+        super();
+        setTimeout(
+            () => { this.foo = 
+                {"firstName":"Tom","lastName":"Wu","username":"tom.wu","email":"tom.wu@genie-networks.com"}},
+            3000
+        )
+        setTimeout(
+            () => { this.foo = {...this.foo, firstName: "Sam"}},
+            5000
+        )
     }
 }
